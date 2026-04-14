5142f49bc06f7e4db491eda84c0bd847c1fc15bb
Christopher Maldonado <cm606434@digicelgroup.com>
2026-04-13 19:04:19 -0600

    ya arreglamos el menu dinamico por roles

--- src/main/webapp/app/entities/entities-menu.component.ts
+++ src/main/webapp/app/entities/entities-menu.component.ts
@@ -1,12 +1,50 @@
-import { defineComponent } from 'vue';
+// import { defineComponent } from 'vue';
+// import { useI18n } from 'vue-i18n';
+
+// export default defineComponent({
+//   name: 'EntitiesMenu',
+//   setup() {
+//     const i18n = useI18n();
+//     return {
+//       t$: i18n.t,
+//     };
+//   },
+// });
+import { defineComponent, inject, computed } from 'vue'; // Añadimos computed
 import { useI18n } from 'vue-i18n';
 
 export default defineComponent({
+  compatConfig: { MODE: 3 },
   name: 'EntitiesMenu',
   setup() {
-    const i18n = useI18n();
+    const t$ = useI18n().t;
+    const accountService = inject<any>('accountService');
+
+    // Usamos una propiedad computada para que Vue reaccione cuando el usuario se loguee
+    const hasAnyAuthority = (authorities: string[] | string): boolean => {
+      // 1. Verificamos si el servicio existe
+      if (!accountService) return false;
+
+      // 2. Si el usuario no está autenticado, no ve nada
+      if (!accountService.authenticated) return false;
+
+      // 3. Intento de usar el método nativo de JHipster
+      if (typeof accountService.hasAnyAuthority === 'function') {
+        return accountService.hasAnyAuthority(authorities);
+      }
+
+      // 4. Plan B: Si lo anterior falla, revisamos los roles manualmente en el store
+      const userAuthorities = accountService.userAuthorities || [];
+      if (typeof authorities === 'string') {
+        return userAuthorities.includes(authorities);
+      } else {
+        return authorities.some(auth => userAuthorities.includes(auth));
+      }
+    };
+
     return {
-      t$: i18n.t,
+      hasAnyAuthority,
+      t$,
     };
   },
 });
--- src/main/webapp/app/entities/entities-menu.vue
+++ src/main/webapp/app/entities/entities-menu.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <b-dropdown-item to="/alma">
+    <b-dropdown-item to="/alma" v-if="hasAnyAuthority(['ROLE_ADMIN', 'ROLE_L2'])">
       <font-awesome-icon icon="asterisk" />
       <span>{{ t$('global.menu.entities.alma') }}</span>
     </b-dropdown-item>
-    <b-dropdown-item to="/cell-type">
+    <b-dropdown-item to="/cell-type" v-if="hasAnyAuthority('ROLE_ADMIN')">
       <font-awesome-icon icon="asterisk" />
       <span>{{ t$('global.menu.entities.cellType') }}</span>
     </b-dropdown-item>
-    <b-dropdown-item to="/cell">
+    <b-dropdown-item to="/cell" v-if="hasAnyAuthority(['ROLE_ADMIN', 'ROLE_L2'])">
       <font-awesome-icon icon="asterisk" />
       <span>{{ t$('global.menu.entities.cell') }}</span>
     </b-dropdown-item>
@@ -16,15 +16,15 @@
       <font-awesome-icon icon="asterisk" />
       <span>{{ t$('global.menu.entities.member') }}</span>
     </b-dropdown-item>
-    <b-dropdown-item to="/iglesia">
+    <b-dropdown-item to="/iglesia" v-if="hasAnyAuthority('ROLE_ADMIN')">
       <font-awesome-icon icon="asterisk" />
       <span>{{ t$('global.menu.entities.iglesia') }}</span>
     </b-dropdown-item>
-    <b-dropdown-item to="/privilege">
+    <b-dropdown-item to="/privilege" v-if="hasAnyAuthority('ROLE_ADMIN')">
       <font-awesome-icon icon="asterisk" />
       <span>{{ t$('global.menu.entities.privilege') }}</span>
     </b-dropdown-item>
-    <b-dropdown-item to="/alma-history">
+    <b-dropdown-item to="/alma-history" v-if="hasAnyAuthority(['ROLE_ADMIN', 'ROLE_L2'])">
       <font-awesome-icon icon="asterisk" />
       <span>{{ t$('global.menu.entities.almaHistory') }}</span>
     </b-dropdown-item>
@@ -36,7 +36,7 @@
       <font-awesome-icon icon="asterisk" />
       <span>{{ t$('global.menu.entities.planificacion') }}</span>
     </b-dropdown-item>
-    <b-dropdown-item to="/member-celula">
+    <b-dropdown-item to="/member-celula" v-if="hasAnyAuthority(['ROLE_ADMIN', 'ROLE_L2'])">
       <font-awesome-icon icon="asterisk" />
       <span>{{ t$('global.menu.entities.memberCelula') }}</span>
     </b-dropdown-item>
@@ -44,7 +44,7 @@
       <font-awesome-icon icon="asterisk" />
       <span>{{ t$('global.menu.entities.planiMaster') }}</span>
     </b-dropdown-item>
-    <b-dropdown-item to="/rol-celula">
+    <b-dropdown-item to="/rol-celula" v-if="hasAnyAuthority('ROLE_ADMIN')">
       <font-awesome-icon icon="asterisk" />
       <span>{{ t$('global.menu.entities.rolCelula') }}</span>
     </b-dropdown-item>
